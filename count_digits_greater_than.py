2d45af42e206c53851c343f0dd862fdcdcf5a9b2
Ilyas Hamada <ilyashamada@Ilyass-MacBook-Air.local>
2025-11-03 21:50:35 +0100

    done

--- count_digits_greater_than.py
+++ count_digits_greater_than.py
@@ -1,4 +1,17 @@
 def count_digits_greater_than(n, t):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    if type (n) != int or type (t) != int:
+        return -1
+    if n<0:
+        return -1
+    if t<0 or t>9 :
+        return -1
+    n = str(n)
+    count = 0
+    for i in n:
+        if int(i)>t:
+            count = count + 1
+    return count
+
+print(count_digits_greater_than(351292, 3))
+print(count_digits_greater_than(888, 8))
+print(count_digits_greater_than(-45, 3))
--- is_strictly_increasing_digits.py
+++ is_strictly_increasing_digits.py
@@ -1,4 +1,35 @@
 def is_strictly_increasing_digits(n):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+
+    if type (n) != int:
+        return -1 
+    if n < 0 :
+        return -1 
+    
+    n = str(n)
+    if len(n) == 1:
+        return True
+    i = 0
+    
+    while True:
+        if i + 1 == len(n):
+            break
+        if n[i] >= n[i + 1]:
+            return False
+    
+        i = i+1
+    return True
+
+
+print(is_strictly_increasing_digits(1234))
+print(is_strictly_increasing_digits(-1234))
+print(is_strictly_increasing_digits(321))
+print(is_strictly_increasing_digits(8.91234))
+
+
+
+
+#1) is_strictly_increasing_digits(n)
+   #- Return True if the digits of a non-negative integer n are strictly increasing from left to right
+  # - Return False otherwise
+   #- Return -1 if n is negative or not an intiger
+   
